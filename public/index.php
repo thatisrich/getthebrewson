2a950ade8235cd128e9cbbf4f307e706adc50c9c
Adam Gowland <adam.gowland@gmail.com>
2014-04-16 12:12:14 +0100

    Cookie (none munchable) functionality added, remembers who is in tea club.

--- public/index.php
+++ public/index.php
@@ -14,13 +14,79 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 	
 	<script type="text/javascript">
-		$(document).ready(function() {
+		function createCookie(name,value,days) {
+			if (days) {
+				var date = new Date();
+				date.setTime(date.getTime()+(days*24*60*60*1000));
+				var expires = "; expires="+date.toGMTString();
+			}
+			else var expires = "";
+			document.cookie = name+"="+value+expires+"; path=/";
+		}
+
+		function readCookie(name) {
+			var nameEQ = name + "=";
+			var ca = document.cookie.split(';');
+			for(var i=0;i < ca.length;i++) {
+				var c = ca[i];
+				while (c.charAt(0)==' ') c = c.substring(1,c.length);
+				if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+			}
+			return null;
+		}
+
+		function eraseCookie(name) {
+			createCookie(name,"",-1);
+		}
 		
+		$(document).ready(function() {
+			
+			var id = 1;
+			var oButton = document.getElementById('addData');
+			var data = document.getElementById('dataInput');
+			var display = document.getElementById('brewers');
+			var brewerscookie = readCookie("brewmaker");
+			
+			console.log(readCookie("brewmaker")); 
+			
+			oButton.onclick = function() {
+				display.innerHTML += "<li class='brewer-" + id + "'>" + data.value + "</li>";
+				if(brewerscookie){
+					createCookie("brewmaker", brewerscookie + "," + data.value, 30);
+				}else{
+					createCookie("brewmaker", data.value, 30);
+				}
+				
+			}
+			
+			
+			if(brewerscookie){
+				var brewers = brewerscookie.split(",");
+				var arrayLength = brewers.length;
+				for (var i = 0; i < arrayLength; i++) {
+				    display.innerHTML += "<li class='brewer-" + id + "'>" + brewers[i] + "</li>";
+				    //Do something
+					id++;
+					if(id >= 3 ) {
+						$("#brews").addClass("brewme");
+						$("#startover").addClass("startover");
+					}
+				}
+				
+			}
 			$("#addData").click(function() {
 				id++;
 				if($('#brewers li').length > 1 ) {
 					$("#brews").addClass("brewme");
+					$("#startover").addClass("startover");
 				}
+				//Empty the text field - speeds up adding names
+				$('#dataInput').val("");
+			});
+			
+			$("#startover").click(function() {
+				eraseCookie("brewmaker");
+				location.reload();
 			});
 			
 			$("#brews").click(function() {
@@ -53,19 +119,10 @@
 		<ul id="brewers" class="brewerlist"></ul>
 		
 		<input id="dataInput" type="text" placeholder="Add another name" /> <button id="addData" type="button">Add it!</button>
-		<script type="text/javascript">
-		
-			var id = 1;
-			var oButton = document.getElementById('addData');
-			var data = document.getElementById('dataInput');
-			var display = document.getElementById('brewers');
-			
-			oButton.onclick = function() {
-				display.innerHTML += "<li class='brewer-" + id + "'>" + data.value + "</li>";
-			}
-		</script>
 		
 		<input id="brews" type="submit" value="Get The Brews On Already!" />
+		
+		<input id="startover" type="submit" value="Start Over" />
 		
 	</section>
 	
