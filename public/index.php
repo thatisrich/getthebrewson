25ad9f3ba53f4c24f820ff8ed5e6894f2909ef89
Richard Walker <richard@monkeybruiser.co.uk>
2016-08-10 15:33:49 +0100

    Adding in tag manager code

--- public/index.php
+++ public/index.php
@@ -12,6 +12,16 @@
 
 	<link href='http://fonts.googleapis.com/css?family=Amaranth:400,700italic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="library/css/style.css">
+	
+	<!-- Google Tag Manager -->
+	<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-N2M8QZ"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-N2M8QZ');</script>
+	<!-- End Google Tag Manager -->
 
 </head>
 <body>
